eb33dd9b4cf495db0706b5edb8ec4f783963e370
Vphadnis <info@viliney.com>
2018-08-16 23:13:01 +0530

    init package

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import sys, os
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+
+version = '1'
+
+install_requires = [
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+]
+
+
+setup(name='spacex-python',
+    version=version,
+    description="Simple Python wrapper for the SpaceX API",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    ],
+    keywords='wrapper api spacex',
+    author='Vinay Phadnis',
+    author_email='phadnisvinay30@gmail.com',
+    url='',
+    license='MIT',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},include_package_data=True,
+    zip_safe=False,
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts':
+            ['spacex-python=spacexpython:main']
+    }
+)
